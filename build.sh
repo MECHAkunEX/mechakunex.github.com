e2a7c81d67e204860814a5d7202e4b1e7e06e0bd
MECHAkunEX <MECHAkunEX@gmail.com>
2012-04-09 23:54:04 -0600

    .

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 export DONT_RANK=1
-game-rank
-~/sbin/org2blog.rkt
-time rake generate
-rake deploy
-git add .
-git commit -m . .
+game-rank && \
+~/sbin/org2blog.rkt && \
+time rake generate && \
+rake deploy && \
+git add . && \
+git commit -m . . && \
 git push origin source
