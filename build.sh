a68cfc40fa876fc621ea5ef0d9e426279471c824
MECHAkunEX <MECHAkunEX@gmail.com>
2012-05-15 06:08:13 -0600

    .

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd ~/Dev/scm/blogs/mechakunex.github.com
 export DONT_RANK=1
 
 game-rank && \
-~/sbin/org2blog.rkt && \
+rk ~/sbin/org2blog.rkt && \
 time rake generate && \
 rake deploy && \
 git add . && \
