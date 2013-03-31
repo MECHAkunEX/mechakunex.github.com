14bb4bbd4e8a3fb79bab7ae0c593300f6d154508
MECHAkunEX <MECHAkunEX@gmail.com>
2013-03-31 17:20:07 -0600

    Update

--- jquery-games.js
+++ jquery-games.js
@@ -1,11 +1,25 @@
 $(window).load(function(){
     var $rows = $('#games tbody tr');
-    $('#filter').keyup(function() {
-        var val = $.trim($(this).val()).replace(/ +/g, ' ');
+    $('#gamefilter').keyup(function() {
+        var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
 
         $rows.show().filter(function() {
-            var text = $(this).text().replace(/\s+/g, ' ');
+            var cells = this.getElementsByTagName('td');
+            var game = cells[2];
+            var text = $(game).text().replace(/\s+/g, ' ').toLowerCase();
             return !~text.indexOf(val);
         }).hide();
     });
+
+    $('#sysfilter').keyup(function() {
+        var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+
+        $rows.show().filter(function() {
+            var cells = this.getElementsByTagName('td');
+            var game = cells[1];
+            var text = $(game).text().replace(/\s+/g, ' ').toLowerCase();
+            return ! (text.substring(0,val.length) == val);
+        }).hide();
+    });
+
 });
